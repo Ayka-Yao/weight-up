3f38b5f45512a97ea0608588494fc4a0a1442ca7
LighT of HeaveN <jeric.ggonzales.dev@gmail.com>
2025-12-11 22:16:38 +0800

    feat connect auth to database

--- server/auth/auth.ts
+++ server/auth/auth.ts
@@ -3,6 +3,22 @@ import { drizzleAdapter } from "better-auth/drizzle";
 import { db } from "../db/index";
 
 
+// for better auth
 export const auth = betterAuth({
-    
-})
+    database: drizzleAdapter(db,{
+        provider:"sqlite"
+    }),
+    emailAndPassword: {
+        enabled:true
+    },
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRECT!,
+            enabled: true
+        }
+    },
+    secret: process.env.BETTER_AUTH_SECRET!,
+    baseURL: process.env.BETTER_AUTH_URL!,
+});
+
