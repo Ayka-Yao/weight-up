b058a691a9a451d53fb4417bcfeaea72a6ac3317
LighT of HeaveN <jeric.ggonzales.dev@gmail.com>
2025-12-17 21:57:23 +0800

    feat: add on schema for Better Auth

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -1,3 +1,4 @@
+import { table } from 'console';
 import { relations, sql } from 'drizzle-orm';
 import { integer, sqliteTable, text, index } from 'drizzle-orm/sqlite-core';
 
@@ -23,18 +24,60 @@ export const session = sqliteTable(
         createdAt: integer("created_at", { mode: "timestamp_ms" })
         .default(sql`(cast(unixepoch('subsecond') * 1000 as integer))`)
         .notNull(),
-    }
-)
-export const accountTable = sqliteTable('account_table',{
-    id: text('id').primaryKey(),
-    userId: integer('user_id').notNull().references(() => user.id),
-    provider: text('provider').notNull(),
-    providerAccountId: text('provider_account_id').notNull(),
-});
+        updatedAt: integer("updated_at", { mode: "timestamp_ms" })
+        .$onUpdate(() => new Date())
+        .notNull(),
+        ipAddress: test("ip_address"),
+        userAgent: text("user_agent"),
+        userId: text("user_id")
+            .notNull()
+            .references(() => user.id, { onDelete: "cascade" }),
+    },
+    (table) => [index("session_userId_idx").on(table.userId)],
+);
+export const account = sqliteTable(
+    "account",
+    {
+    id: text("id").primaryKey(),
+    accountId: text("account_id").notNull,
+    providerId: text("provider_id").notNull(),
+    userId: text("user_id")
+        .notNull()
+        .references(() => user.id, { onDelete: "cascade" }),
+    accessToken: text("access_token"),
+    refreshToken: text("refresh_token"),
+    idToken: text("id_token"),
+    accessTokenExpiresAt: integer("refresh_token_expires_at",{
+        mode: "timestamp_ms",
+    }),
+    scope: text("scope"),
+    password: text("password"),
+    createdAt: integer("created_at", { mode: "timestamp_ms" })
+        .default(sql`(cast(unixepoch('subsecond') * 1000 as integer))`)
+        .notNull(),
+
+    },
+    (table) => [index("account_userId_idx").on(table.userId)],
+);
+
+export const verification = sqliteTable(
+    "verification",
+    {
+        id: text("id").primaryKey(),
+        identifier: text("identifier").notNull(),
+        value: text("value").notNull(),
+        expiresAt: integer("expires_at", { mode: "timestamp_ms" }).notNull(),
+        createdAt: integer("created_at", { mode: "timestamp_ms"})
+        .default(sql`(cast(unixepoch('subsecond') * 1000 as integer))`)
+        .notNull(),
+
+    },
+    (table) => [index("verification_identifier_idx").on(table.identifier)],
+);
 
 export const weightEntries = sqliteTable('weight_entries',{
     id: integer('id').primaryKey({ autoIncrement: true }),
-    userId: integer('user_id').notNull().references(() => user.id),
+    userId: text('user_id').notNull().references(() => user.id),
     date: integer('date', { mode: 'timestamp' }).notNull(),
     notes: text('notes'),
     createdAt: integer('created_at', { mode: 'timestamp' })
